54e69f0d3db9caea35669d0ee7481a36efcb4b0f
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2023-10-25 11:43:52 -0500

    documentacion de la clase email por yeimer sebastian

--- app/libs/Email.php
+++ app/libs/Email.php
@@ -1,16 +1,36 @@
 <?php
 
 namespace Adso\libs;
+
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 
+/**
+ * Clase para enviar el correo electronico  para la recuperacion de la contraseña
+ */
+
 class Email {
+
+
+     /**
+     * Envía un correo electrónico a la dirección especificada con un enlace para actualizar la contraseña.
+     *
+     * hacemos un try catch en el cual modificamos el puerto del servidor SMTP 
+     * despues colocamos quien lo envia y a quien se lo enviamos 
+     * despues creamos el cuerpo del gmail que le va a llegar al destinatario 
+     * despues creamos una Exception la cual si esta erronio el correo nle aparezca un mensage de advertencia 
+     * 
+     * @param string $email La dirección de correo electrónico del destinatario.
+     * @param int $id El identificador que se usará en el enlace para actualizar la contraseña.
+     * @return void 
+     *
+     */
 
     function sendEmail($email, $id) {
         $mail = new PHPMailer(true);
         
         try {
-            //Server settings
+            // Configuración del servidor SMTP y credenciales de autenticación
             $mail->isSMTP();
             $mail->Host = 'sandbox.smtp.mailtrap.io';
             $mail->SMTPAuth = true;
@@ -18,9 +38,10 @@ class Email {
             $mail->Username = '985fa8dd2f8e5d';
             $mail->Password = '8d66f38b55a76e';
     
-            //Recipients
+            //aqui colocamos la dirrecion gmail de quien lo envia 
             $mail->setFrom('liamreyes2704@gmail.com', 'Liam Reyes');
             // $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+            //aqui colocamos en destinatario 
             $mail->addAddress($email);               //Name is optional
             // $mail->addReplyTo('info@example.com', 'Information');
             // $mail->addCC('cc@example.com');
@@ -30,15 +51,17 @@ class Email {
             // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
             // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
     
-            //Content
+            //Content aqui hacemos el cuerpo del gmail que le va a llegar al destinatario  
             $mail->isHTML(true);                                  //Set email format to HTML
             $mail->Subject = 'Password update request';
             $mail->Body    = 'Ingresa a este <a href="'.URL.'/login/updatepassword/'.$id.'">link</a> si deseas cambiar tu contraseña';
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
             $mail->send();
         } catch (Exception $e) {
+            //aqui creamos la Exception para que si esta erronio el gamil aparaezca el mensaje 
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
+
     }
 
 }
