12736e8ae57fd4f70c656a01d8d3447b0cf7bdc0
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2023-10-25 11:18:34 -0500

    Documentacion de PermissionController

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -5,6 +5,13 @@ namespace Adso\controllers;
 use Adso\Libs\controller;
 use Adso\libs\Permisson;
 
+/**
+ * Clase AdminController
+ * 
+ * Esta clase reprensenta el controlador 
+ * 
+ * 
+ */
 class AdminController extends Controller
 {
     function __construct()
--- app/controllers/PermissonController.php
+++ app/controllers/PermissonController.php
@@ -4,18 +4,36 @@ namespace Adso\controllers;
 
 use Adso\libs\Controller;
 use Adso\libs\Helper;
-
+/**
+     * Clase PermissonController 
+     * 
+     * El controlador PermissonController maneja todas las acciones relacionadas con los permisos en la pagina. Esto incluye la creación, edición, eliminación y visualización de permisos. Además, se encarga de almacenar nuevos permisos en la base de datos y actualizar permisos existentes.
+     */
 class PermissonController extends Controller
 {
-
+ 
     protected $model = "";
 
+    /**
+     * Constructor de PermissonController.
+     * 
+     * Este método se encarga de inicializar el controlador, estableciendo una instancia del modelo de permisos para su uso posterior.
+     */
     function __construct()
     {
 
         $this->model = $this->model('Permisson');
     }
 
+     /**
+     * Acción Index
+     *
+     * Este método maneja la acción de mostrar la lista de permisos en la página.
+     * 
+     *Recupera la lista de permisos desde el modelo, prepara los datos necesarios para la vista y luego muestra la vista que presenta los permisos al usuario. Esta acción proporciona una visión general de los permisos disponibles en la aplicación.
+     * @access public
+     * @return void
+     */
     function index()
     {
         $permisos = $this->model->getPermisson();
@@ -30,6 +48,16 @@ class PermissonController extends Controller
         $this->view('permisson/index', $data, 'app');
     }
 
+    /**
+     * Acción Create
+     *
+     * Este método maneja la acción de mostrar la vista para crear un nuevo permiso.
+     * 
+     *  Prepara los datos necesarios para la vista, establece el título y el subtítulo, y luego muestra la vista que permite al usuario crear un nuevo permiso en la aplicación.
+     *
+     * @access public
+     * @return void
+     */
     function create()
     {
         $data = [
@@ -40,6 +68,16 @@ class PermissonController extends Controller
 
         $this->view('permisson/create', $data, 'app');
     }
+    /**
+     * Acción Storage
+     *
+     * Este método maneja el almacenamiento de un nuevo permiso en la base de datos.
+     * 
+     * Si se recibe una solicitud POST, valida los datos enviados, como el nombre del permiso. Si los datos son válidos, almacena el nuevo permiso en la base de datos y redirige al usuario a la página de permisos. Si hay errores en los datos, muestra la vista de creación nuevamente con los mensajes de error correspondientes.
+     *
+     * @access public
+     * @return void
+     */
 
     function storage()
     {
@@ -77,6 +115,17 @@ class PermissonController extends Controller
             }
         }
     }
+    /**
+     * Acción Editar
+     *
+     * Este método maneja la acción de mostrar la vista de edición de un permiso existente.
+     *
+     * Recibe un ID de permiso como parámetro, recupera los datos del permiso correspondiente desde el modelo y prepara los datos necesarios para la vista de edición. Luego, muestra la vista que permite al usuario editar el permiso existente.
+     * 
+     * @param string $id ID del permiso a editar.
+     * @access public
+     * @return void
+     */
 
     function editar($id)
     {
@@ -95,6 +144,19 @@ class PermissonController extends Controller
 
         $this->view('permisson/update', $data, 'app');
     }
+
+    /**
+     * Acción Update
+     *
+     * Este método maneja la actualización de un permiso existente en la base de datos.
+     * 
+     *  Recibe un ID de permiso como parámetro y, si se recibe una solicitud POST, valida los datos enviados, como el nombre del permiso. Si los datos son válidos, actualiza el permiso existente en la base de datos y redirige al usuario a la página de permisos. Si hay errores en los datos, muestra la vista de edición nuevamente con los mensajes de error correspondientes.
+     *
+     * @param string $id ID del permiso a actualizar.
+     * @access public
+     * @return void
+     */
+
     function update($id){
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
                         
@@ -135,6 +197,18 @@ class PermissonController extends Controller
         }
         
     }
+
+    /**
+     * Acción Delete
+     *
+     * Este método maneja la eliminación de un permiso de la base de datos.
+     * 
+     * Recibe un ID de permiso como parámetro y elimina el permiso correspondiente en la base de datos. Luego, redirige al usuario a la página de permisos, mostrando la lista actualizada de permisos sin el permiso eliminado.
+     *
+     * @param string $id ID del permiso a eliminar.
+     * @access public
+     * @return void
+     */
     function delete($id)
     {
                 
