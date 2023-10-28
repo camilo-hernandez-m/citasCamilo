ad396ce6f2027521fd39549c6297ca969e3d18f9
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-10-28 18:01:11 -0500

    DataHelper Comentado

--- app/libs/DateHelper.php
+++ app/libs/DateHelper.php
@@ -6,27 +6,34 @@ use Carbon\Carbon;
 use DateTimeZone;
 use PSpell\Config;
 
+/**
+ * La clase DateHelper proporciona funciones para trabajar con fechas y horas.
+ */
 class DateHelper {
 
-    public static function shortDate($date = ""){
-        $carbon = new Carbon(); 
+    /**
+     * Convierte una fecha en un formato corto personalizado y la devuelve.
+     *
+     * @param string $date La fecha que se desea formatear (en formato ISO 8601).
+     * @return string La fecha en el formato personalizado "D/MM/Y".
+     */
+
+    public static function shortDate($date = "") {
+        $carbon = new Carbon();
 
         $fecha = Carbon::parse($date)->locale('es');
-        // $fecha = Carbon::createFromFormat('Y-');
-        return $fecha -> isoFormat("D/MM/Y");
+        return $fecha->isoFormat("D/MM/Y");
     }
+
+    /**
+     * Calcula la diferencia en segundos entre la hora proporcionada y la hora actual en Bogotá.
+     *
+     * @param string $hora La hora que se desea comparar (en formato ISO 8601).
+     * @return int La diferencia en segundos entre las dos horas.
+     */
     public static function timestamp($hora) {
-        
-
-        // $horaEs = Carbon::parse($hora)->locale('es');
-        // print_r($hora);
-        // die();
         $fecha = Carbon::now('America/Bogota');
-
-        $diff = $fecha -> diffInSeconds($hora);
-        // print_r($diff);
-        // die();
+        $diff = $fecha->diffInSeconds($hora);
         return $diff;
-
     }
 }
